004eb65c4ac96a8cca7beb8fd63c8600c2032205
TongyunLyu <lvtongyun9191@gmail.com>
2015-04-29 12:17:40 -0400

    server_toast

--- src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
@@ -175,6 +175,20 @@ public class ChatServerImpl extends Thread implements ChatServer {
             // server. HINT: This will look very similar to onNewMessage but
             // instead of notifying the clients of the new message, it will
             // notify them of a joining user.
+            synchronized (clients) {
+                for (Socket s : clients) {
+                    try {
+                        ObjectOutputStream out = new ObjectOutputStream(
+                                s.getOutputStream());
+                        String str1 = username + " has joined the server.";
+                        String str2 = "SERVER";
+                        Message msg = new Message(str1, str2);
+                        out.writeObject(msg);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Unable to send message to client.");
+                    }
+                }
+            }
         }
 
 
